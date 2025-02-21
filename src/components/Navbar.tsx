83213540289646a513f4a78429cf89c1c10b9302
Primqd <franklinvchen@outlook.com>
2025-02-21 00:29:03 -0800

    got navbar wokring

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,43 @@
 import { Link } from "react-router-dom";
+import debugNavbarArt from "../assets/debug_navbar_art.png"
 import "../index.css";
 
 {/* <p className="text-blue-600/100 dark:text-sky-400/100 bg-gray-800 p-2">The quick brown fox...</p>
 <p className="bg-gradient-to-r from-blue-500 via-teal-500 to-green-500">The quick brown fox...</p> */}
 
+const buttonStyle = "text-white";
+
 function Navbar() {
     return (
-        <nav>
-
+        <nav className="bg-white border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+            <div className="h-8 overflow-hidden flex relative items-center justify-center mx-auto"> {/* top header stuff*/}
+                <img className="" src={debugNavbarArt} alt="generic navbar art"/>
+                <Link to="/" className="font-semibold absolute text-center top-0 z-1 text-white">frankvc.com</Link>
+            </div>
+            <div className="max-w-screen-xl flex p-2 content-center">
+                <ul className="flex w-full justify-evenly items-center">
+                    <li className={buttonStyle}>
+                        <Link to="/">
+                            <input type="button" value="Home" />
+                        </Link>
+                    </li>
+                    <li className={buttonStyle}>
+                        <Link to="/games">
+                            <input type="button" value="Games" />
+                        </Link>
+                    </li>
+                    <li className={buttonStyle}>
+                        <Link to="/projects">
+                            <input type="button" value="Projects" />
+                        </Link>
+                    </li>
+                    <li className={buttonStyle}>
+                        <Link to="/about">
+                            <input type="button" value="About" />
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
 
     return <>
         <h1 className="flex justify-center">Currently under work... part 3</h1>
-        <input type="button" value={`value = ${num}`} onClick={() => (setNum(num + 1))} />
+        <input className="bg-blue-500" type="button" value={`value = ${num}`} onClick={() => (setNum(num + 1))} />
     </>
 }
 
