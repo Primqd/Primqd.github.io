cc83666426ef8c7b977997abe2284590a7109675
Primqd <franklinvchen@outlook.com>
2025-02-22 01:17:43 -0800

    added stupid button to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 
+const buttonLines = [
+    'Oof!',
+    'Ouch!',
+    'Stop that!',
+    "I'm warning you!",
+    'Hey!'
+]
+
 function Home() {
     const [num, setNum] = useState(0);
 
     return <>
         <h1 className="flex justify-center">welcome home</h1>
-        <input type="button" value={`clicker = ${num}`} onClick={() => (setNum(num + 1))} />
+        <input className="z-1 w-[25vw] h-[25vh] text-white transition duration-300 bg-pink-600 hover:bg-pink-400 active:bg-pink-200" type="button" value={num ? buttonLines[num % buttonLines.length] : "Don't touch me!"} onClick={() => (setNum(num + 1))} />
     </>
 }
 
