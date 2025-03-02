96432a7a8f8aa51ab1e37307c8c075668cc9130b
Primqd <franklinvchen@outlook.com>
2025-03-01 20:14:53 -0800

    removed home test code and changed color scheme to neutral

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
-
-const buttonLines = [
-    'Oof!',
-    'Ouch!',
-    'Stop that!',
-    "I'm warning you!",
-    'Hey!'
-]
-
 function Home() {
-    const [num, setNum] = useState(0);
-
-    return <>
-        <h1 className="flex justify-center">welcome home</h1>
-        <input className="z-1 w-[25vw] h-[25vh] text-white transition duration-300 bg-pink-600 hover:bg-pink-400 active:bg-pink-200" type="button" value={num ? buttonLines[num % buttonLines.length] : "Don't touch me!"} onClick={() => (setNum(num + 1))} />
-    </>
+    return <div className="bg-neutral-800 text-white pt-[8vh] h-[100vh]">
+        <div className="">
+            <h1>Hi</h1>
+        </div>
+    </div>
 }
 
 export default Home;
