25fc1592284d1a618b962b3b82e7b0c6dac29e9a
Primqd <franklinvchen@outlook.com>
2025-03-01 20:56:01 -0800

    finished home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import pig from "../assets/pig.png"
 
 function Home() {
     return <div className="bg-neutral-800 text-white pt-[8vh] h-[100vh]">
-        <div className="flex justify-center gap-[40vw] pt-[16vh]">
-            <div className="yap">
+        <div className="flex justify-center items-center gap-[30vw] pt-[16vh]">
+            <div className="yap align-middle">
                 <h1 className="text-5xl font-semibold pb-[1vh]">Hi!</h1>
                 <p className="max-w-lg text-lg text-gray-400">
                     This is my personal website, where I do all sorts of shenanigans.
@@ -20,9 +21,10 @@ function Home() {
                     <Link className="font-semibold text-white" to="/about"> about </Link> page! 
                 </p>
             </div>
-            <div>
-                hello
-            </div>
+            <figure className="max-w-lg">
+                <img className="mx-auto max-w-full rounded-lg" alt="clay pig" src={pig}></img>
+                <figcaption className="mt-2 text-sm text-center text-gray-400 italic">Picture unrelated.</figcaption>
+            </figure>
         </div>
     </div>
 }
