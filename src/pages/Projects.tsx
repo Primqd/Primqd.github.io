7e3b07a36704f17188be2029eeca962dd6173822
Primqd <franklinvchen@outlook.com>
2025-03-07 00:09:11 -0800

    made project components allign to middle

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -11,12 +11,12 @@ type ProjectProps = {
 function Project({title, description, image, link} : ProjectProps) {
     return(
         <div className="px-[10vw] py-[8vh] w-[97.5vw] h-[47.5vh]">
-            <div className="flex border-4 border-neutral-600 bg-neutral-700">
+            <div className="flex border-4 border-neutral-600 bg-neutral-700 align-middle">
                 <div className="px-[2.5vw] py-[2.5vh] w-[100%]">
-                        <Link to={link} className="text-4xl font-semibold hover:underline py-[1vh] w-fit">
+                        <Link to={link} className="text-5xl font-semibold hover:underline py-[1vh] w-fit">
                             {title}
                         </Link>
-                        <p className="py-[1vh] text-lg text-neutral-400">
+                        <p className="py-[1vh] text-2xl text-neutral-400">
                             {description}
                         </p>
                 </div>
