5954b6e42495dc1e09cbd77e6f413ac7bf9a599a
Primqd <franklinvchen@outlook.com>
2025-03-06 22:28:05 -0800

    added basic project component

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,20 @@
+type ProjectProps = {
+    title : string,
+    description : string,
+    image : string,
+}
+
+function Project({title, description, image} : ProjectProps) {
+    return <div className="justify-center items-center gap-[30vw] px-[8vw]">
+        {title}
+    </div>
+
+}
+
 function Projects() {
-    return <>
-        <h1 className="flex justify-center">man i wish i actually had projects</h1>
-    </>
+    return <div className="bg-neutral-800 text-white pt-[8vh] h-[100vh]">
+        <Project title="hello" description="test" image="test" />
+    </div>
 }
 
 export default Projects;
