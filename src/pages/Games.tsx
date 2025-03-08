f9e4656d77aa07b6ba268d256bde829f69923d32
Primqd <franklinvchen@outlook.com>
2025-03-08 00:03:51 -0800

    copied projects.tsx code to games.tsx and added minimum height to both

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,15 +1,14 @@
+import test from "../assets/debug_navbar_art.png"
+import Project from "../components/Project";
+
 function Games() {
-    return <div className="pt-[8vh]"> {/*change to 8vh once testing is done */}
-        <p>
-            would you rather
-            have unlimited bacon
-            and no games
-        </p>
-        <p className="absolute -bottom-[8vh]">
-            or
-            games, unlimtied games,
-            and no games
-        </p>    
+    return <div className="bg-neutral-800 text-white py-[8vh] min-h-screen">
+        <Project
+            title="Lorem Ipsum" 
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            image={test} 
+            link="/"
+        />
     </div>
 }
 
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,7 +2,7 @@ import test from "../assets/debug_navbar_art.png"
 import Project from "../components/Project";
 
 function Projects() {
-    return <div className="bg-neutral-800 text-white py-[8vh]">
+    return <div className="bg-neutral-800 text-white py-[8vh] min-h-screen">
         <Project
             title="Lorem Ipsum" 
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
