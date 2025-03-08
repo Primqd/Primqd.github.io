6a50a01521d255676dad2939f440b546e3b88021
Primqd <franklinvchen@outlook.com>
2025-03-08 00:15:22 -0800

    added temporary games page

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,14 +1,9 @@
-import test from "../assets/debug_navbar_art.png"
-import Project from "../components/Project";
+// import Project from "../components/Project";
 
 function Games() {
     return <div className="bg-neutral-800 text-white py-[8vh] min-h-screen">
-        <Project
-            title="Lorem Ipsum" 
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            image={test} 
-            link="/"
-        />
+        <h1 className=" py-[8vh] h-[80vh] flex items-center justify-center">Nothing yet! Check back later...</h1>
+        <h1 className="absolute bottom-5 left-1/2 text-neutral-700 transform -translate-x-1/2">Would you rather have unlimited bacon but no more video games or games, unlimited games, but no more games?</h1>
     </div>
 }
 
