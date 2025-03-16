91b1a684fa7d81d36d5b8b0f78cc6720a695fcb6
Primqd <franklinvchen@outlook.com>
2025-03-16 14:36:45 -0700

    made home page use relative values

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 import pig from "../assets/pig.png"
 
 function Home() {
-    return <div className="bg-neutral-800 text-white pt-[8vh] h-[100vh]">
-        <div className="flex justify-center items-center gap-[30vw] px-[8vw] pt-[16vh]">
+    return <div className="bg-neutral-800 text-white pt-[8vh] min-h-screen">
+        <div className="flex justify-center items-center gap-[35%] px-[8vw] pt-[16vh]">
             <div className="yap align-middle">
                 <h1 className="text-5xl font-semibold pb-[1vh]">Hi!</h1>
                 <p className="max-w-lg text-lg text-gray-400">
