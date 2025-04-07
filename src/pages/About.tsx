fafcd7ab5873613ffaff2256265d171ada2f2b28
Primqd <franklinvchen@outlook.com>
2025-04-06 19:55:36 -0700

    added sample text to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,19 @@
 function About() {
-    return <div className="pt-[8vh]">
-        <h1 className="flex justify-center">man i wish i had a personality</h1>
+    return <div className="bg-neutral-800 text-white pt-[8vh] min-h-screen">
+        <div className="flex px-[8vh] pt-[8vh]">
+            <div className="yap align-middle">
+                <h1 className="text-5xl font-semibold pb-[1vh]">Hi!</h1>
+                <p className="max-w-lg text-lg text-gray-400">
+                    This is my personal website, where I do all sorts of shenanigans.
+                </p>
+                <p className="max-w-lg text-lg text-gray-400">
+                    Check out the pages to see more.
+                </p>
+                <p className="max-w-lg text-lg text-gray-400">
+                    If you'd like to learn more about me, check out my
+                </p>
+            </div>
+        </div>
     </div>
 }
 
