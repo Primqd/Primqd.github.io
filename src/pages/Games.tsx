e0ec9080712b21a31c97c492428f224f1e488d3c
Primqd <franklinvchen@outlook.com>
2025-04-06 19:37:59 -0700

    fixed goofy game text

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,9 +1,9 @@
 // import Project from "../components/Project";
 
 function Games() {
-    return <div className="bg-neutral-800 text-white pt-[8vh] min-h-screen">
-        <h1 className=" py-[8vh] h-[80vh] flex items-center justify-center">Nothing yet! Check back later...</h1>
-        <h1 className="absolute bottom-5 left-1/2 text-neutral-700 transform -translate-x-1/2">Would you rather have unlimited bacon but no more video games or games, unlimited games, but no more games?</h1>
+    return <div className="bg-neutral-800 text-white pt-[8vh] min-h-screen items-center">
+        <h1 className=" py-[8vh] h-[80vh] flex text-center items-center justify-center">Nothing yet! Check back later...</h1>
+        <h1 className="text-neutral-500 text-center flex justify-center">Would you rather have unlimited bacon but no more video games or games, unlimited games, but no more games?</h1>
     </div>
 }
 
