98d22ccf96feab2a16b1314a3e68f05979f0cf30
Primqd <franklinvchen@outlook.com>
2025-04-09 00:41:48 -0700

    changed game text to italic

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -3,7 +3,7 @@
 function Games() {
     return <div className="bg-neutral-800 text-white pt-[5rem] min-h-screen items-center">
         <h1 className=" py-[5rem] h-[80%] flex text-center items-center justify-center">Nothing yet! Check back later...</h1>
-        <h1 className="text-neutral-500 w-[80%] text-center absolute bottom-5 left-1/2 transform -translate-x-1/2">Would you rather have unlimited bacon but no more video games or games, unlimited games, but no more games?</h1>
+        <h1 className="text-neutral-500 w-[80%] text-center italic absolute bottom-5 left-1/2 transform -translate-x-1/2">Would you rather have unlimited bacon but no more video games or games, unlimited games, but no more games?</h1>
     </div>
 }
 
