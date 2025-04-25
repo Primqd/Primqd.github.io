22e86d9151b710de79ac5da24a4d1dccfcf44f52
Primqd <franklinvchen@outlook.com>
2025-04-24 18:28:57 -0700

    swapped photo header to React.FC

--- src/projects/photo-portfolio/components/header.tsx
+++ src/projects/photo-portfolio/components/header.tsx
@@ -1,10 +1,20 @@
 // a. Title (First and Last name), subtitle (Photographer), header image (must be yours)
 // Artist bio/statement: 90 word min. - type in Microsoft Word to check the word count * (see resources for how to write artist statement
 
-function PhotoHeader() {
-    return <div>
-        <h1>Header</h1>
-    </div>
+type PhotoHeaderProps = { photo: string } // props are good!
+
+// TODO: convert all function components to this style
+export const PhotoHeader: React.FC<PhotoHeaderProps> = (
+    { photo }
+) => {
+
+    return (
+        <div>
+            <h1>Header</h1>
+            <h2>{photo}</h2>
+        </div>);
+
 }
+
 
 export default PhotoHeader
--- src/projects/photo-portfolio/portfolio.tsx
+++ src/projects/photo-portfolio/portfolio.tsx
@@ -2,11 +2,11 @@ import PhotoHacks from "./components/hacks"
 import PhotoHeader from "./components/header"
 import PhotoProjects from "./components/projects"
 import WeeklyPhotos from "./components/wp"
-
+import debug from "../../assets/debug_navbar_art.png"
 
 function PhotoPortfolio() {
     return <div className="">
-        <PhotoHeader />
+        <PhotoHeader photo={debug}/>
         <PhotoProjects />
         <WeeklyPhotos />
         <PhotoHacks />
