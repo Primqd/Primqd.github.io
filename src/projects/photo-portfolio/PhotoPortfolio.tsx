3b460719e33e0819dc58ac11b1d3a5017d249501
Primqd <franklinvchen@outlook.com>
2025-04-25 21:00:05 -0700

    added photobio component to portfolio

--- src/projects/photo-portfolio/PhotoPortfolio.tsx
+++ src/projects/photo-portfolio/PhotoPortfolio.tsx
@@ -2,11 +2,13 @@ import { PhotoHacks } from "./components/PhotoHacks"
 import { PhotoHeader } from "./components/PhotoHeader"
 import { PhotoProjects } from "./components/PhotoProjects"
 import { PhotoWeekly } from "./components/PhotoWeekly"
+import { PhotoBio } from "./components/PhotoBio"
 import pig from "../../assets/pig.png"
 
 export const PhotoPortfolio: React.FC = () => {
     return <div className="">
         <PhotoHeader photo={pig} />
+        <PhotoBio />
         <PhotoProjects />
         <PhotoWeekly />
         <PhotoHacks />
---
+++ src/projects/photo-portfolio/components/PhotoBio.tsx
@@ -0,0 +1,7 @@
+export const PhotoBio: React.FC = () => {
+    return (
+        <div id="photo-bio" className="max-w-screen h-[20rem] relative">
+            <h2 className="text-2xl font-semibold mb-4">Artist Statement</h2>
+        </div>
+    );
+};
