9c2ddb347105d34a363879ddcdc4c1fa813e612b
Primqd <franklinvchen@outlook.com>
2025-05-02 20:26:12 -0700

    replace black div with brightness tailwind

--- src/projects/photo-portfolio/components/PhotoHeader.tsx
+++ src/projects/photo-portfolio/components/PhotoHeader.tsx
@@ -12,8 +12,7 @@ export const PhotoHeader: React.FC<PhotoHeaderProps> = (
     return (
         <div>
             <figure className="max-w-screen h-[20rem] relative">
-                <img src={photo} className="w-full h-[20rem] absolute object-cover object-center" alt="clay pig" />
-                <div id="black-filter" className="w-full h-[20rem] absolute bg-black opacity-35" />
+                <img src={photo} className="w-full h-[20rem] absolute object-cover object-center brightness-65" alt="clay pig" />
                 <h1 id="name" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform text-white font-semibold text-5xl whitespace-nowrap">Franklin Chen</h1>
                 <h1 id="name" className="absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-7 transform text-white text-3xl">Photographer</h1>
                 {/* <figcaption className="mt-2 text-sm text-center text-gray-400 italic">Picture unrelated.</figcaption> */}
