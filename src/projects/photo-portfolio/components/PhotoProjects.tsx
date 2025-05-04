d3ead9b4f4b8b8e2e332b5fd73c71e829a18cca9
Primqd <franklinvchen@outlook.com>
2025-05-03 19:55:48 -0700

    refactor PhotoProjects component to include additional photo projects and improve layout

--- src/projects/photo-portfolio/components/PhotoProjects.tsx
+++ src/projects/photo-portfolio/components/PhotoProjects.tsx
@@ -5,11 +5,41 @@
 // § MPC 3 - Your Choice
 // Final (MPC4) - Your Art, Your Choice
 
+import ChenF_MPC1_Pun_CS from "../../../assets/photo_portfolio/ChenF_MPC1_Pun_CS.jpg"
+import ChenF_MPC1_Pun_Edit_01 from "../../../assets/photo_portfolio/ChenF_MPC1_Pun_Edit_01.jpg"
+import ChenF_MPC1_Pun_Edit_02 from "../../../assets/photo_portfolio/ChenF_MPC1_Pun_Edit_02.jpg"
+
+import ChenF_MPC2_Product_CS from "../../../assets/photo_portfolio/ChenF_MPC2_Product_CS.png"
+import ChenF_MPC2_Product_Edit_02 from "../../../assets/photo_portfolio/ChenF_MPC2_Product_Edit_02.jpg"
+import ChenF_MPC2_Product_Edit_07 from "../../../assets/photo_portfolio/ChenF_MPC2_Product_Edit_07.jpg"
+
+type PhotoProjectProps = {
+    title: string;
+    description: string;
+    contact_sheet: string;
+    edit_1: string;
+    edit_2: string;
+}
+
+const PhotoProject: React.FC<PhotoProjectProps> = ({ title, description, contact_sheet, edit_1, edit_2 }) => {
+    return (
+        <div className="text-left">
+            {title}{description}{contact_sheet}{edit_1}{edit_2}
+        </div>
+    )
+}
 
 export const PhotoProjects: React.FC = () => {
     return (
-        <div>
-            <h1>Projects</h1>
+        <div className="max-w-screen flex flex-col justify-center">
+            <h2 className="py-4 font-semibold text-5xl whitespace-nowrap text-center">Projects</h2>
+            <PhotoProject
+                title="MPC 1: Pun"
+                description="test"
+                contact_sheet={ChenF_MPC1_Pun_CS}
+                edit_1={ChenF_MPC1_Pun_Edit_01}
+                edit_2={ChenF_MPC1_Pun_Edit_02}
+            />
         </div>
     )
 }
