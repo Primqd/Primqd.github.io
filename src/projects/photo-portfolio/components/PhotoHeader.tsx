75c87854a91cfd218e06a1dff32a2601162a9ebc
Primqd <franklinvchen@outlook.com>
2025-05-04 23:51:14 -0700

    changed Photo elemetn in PhotoHeader to new children method

--- src/projects/photo-portfolio/components/PhotoHeader.tsx
+++ src/projects/photo-portfolio/components/PhotoHeader.tsx
@@ -18,8 +18,9 @@ export const PhotoHeader: React.FC<PhotoHeaderProps> = (
                     photo={photo}
                     photo_style="w-full h-[20rem] absolute object-cover object-center brightness-65"
                     title="Clay Pig"
-                    description="A clay pig I got from a yard sale. One of my first photos with my camera."
-                />
+                >
+                    Clay pig I got from a yard sale. See Weekly Photo #2 for more.
+                </Photo>
                 <h1 id="name" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform text-white font-semibold text-5xl whitespace-nowrap">Franklin Chen</h1>
                 <h1 id="name" className="absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-7 transform text-white text-3xl">Photographer</h1>
                 {/* <figcaption className="mt-2 text-sm text-center text-gray-400 italic">Picture unrelated.</figcaption> */}
