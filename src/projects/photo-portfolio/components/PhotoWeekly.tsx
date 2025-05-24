7d1e6dd5e80c645f85daf6ae0e991835d4c5c5ba
Primqd <franklinvchen@outlook.com>
2025-05-23 20:52:20 -0700

    added positioning parameter to WeeklyPhoto subcomponent

--- src/projects/photo-portfolio/components/PhotoWeekly.tsx
+++ src/projects/photo-portfolio/components/PhotoWeekly.tsx
@@ -8,11 +8,12 @@ import { Photo } from "../../../components/Photo"
 type WeeklyPhotoProps = {
     photo: string,
     title: string,
+    position?: string,
 }
 
-const WeeklyPhoto: React.FC<PropsWithChildren<WeeklyPhotoProps>> = ({ photo, title, children }) => {
+const WeeklyPhoto: React.FC<PropsWithChildren<WeeklyPhotoProps>> = ({ photo, title, children, position }) => {
     return (
-        <div className="relative w-[20rem] h-[20rem] m-5">
+        <div className={"relative w-[20rem] h-[20rem] absolute " + position}>
             <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[16rem] text-center text-white font-semibold text-3xl pointer-events-none z-1">{title}</h1>
             <Photo
                 photo={photo}
@@ -28,9 +29,11 @@ const WeeklyPhoto: React.FC<PropsWithChildren<WeeklyPhotoProps>> = ({ photo, tit
 export const PhotoWeekly: React.FC = () => {
     return (
         <div>
+            <h2 className="pb-10 font-semibold text-5xl whitespace-nowrap text-center">Projects</h2>
             <WeeklyPhoto
                 photo={pig}
                 title="WP1: My Life"
+                position="left-[50%] -translate-x-1/2"
             >
                 This is my (unsorted) pile of clothes sitting in my room. I used back lighting to better emphasize the texture on the clothes. I used a bird's eye view to emphasize the overall texture, rather than one specific texture.
             </WeeklyPhoto>
