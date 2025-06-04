3f86043e280faa07dd757dc009c53be574560ac3
Primqd <franklinvchen@outlook.com>
2025-06-03 20:38:28 -0700

    started work on photo hacks page

--- src/projects/photo-portfolio/components/PhotoHacks.tsx
+++ src/projects/photo-portfolio/components/PhotoHacks.tsx
@@ -1,9 +1,23 @@
 // Cell Phone/Camera Hacks section: Caption each (of the 6) with the technique
 
+import { Carousel } from "flowbite-react"
+import { Photo } from "../../../components/Photo"
+
+import ChenF_Hacks_Pinhole from "../../../assets/photo_portfolio/Hacks/ChenF_Hacks_Pinhole.png";
+
 export const PhotoHacks: React.FC = ({ }) => {
     return (
-        <div>
-            <h1>Hacks</h1>
+        <div className="max-w-screen flex flex-col justify-center items-center">
+            <h2 className="pt-4 font-semibold text-5xl whitespace-nowrap text-center">Photo Hacks</h2>
+            <div className="w-[80vw] h-[80vh] pt-4">
+                <Carousel pauseOnHover slideInterval={30000}>
+                    <Photo
+                        photo={ChenF_Hacks_Pinhole}
+                        title="Pinhole Camera"
+                        photo_style="h-[100%] object-contain place-self-center rounded-lg align-top p-16"
+                    />
+                </Carousel>
+            </div>
         </div>
     )
 }
