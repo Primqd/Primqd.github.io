a0f285853a3dcbe2df2c694e68053e572df9f16a
Primqd <franklinvchen@outlook.com>
2025-06-20 19:18:11 -0700

    switched navbar links to flowbite elements for compatability

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
 import "../index.css";
 
 const liStyle = "w-1/4";
-const buttonStyle =
-    "z-1 h-full w-full text-[1.2rem] text-text-highlight dark:text-dark-text-highlight transition duration-300 hover:bg-button-hover dark:hover:bg-dark-button-hover active:bg-button-click dark:active:bg-dark-button-click";
+const buttonStyle = "z-1 h-full w-full text-[1.2rem] text-text-highlight dark:text-dark-text-highlight transition duration-300 hover:bg-button-hover dark:hover:bg-dark-button-hover active:bg-button-click dark:active:bg-dark-button-click";
 
+// TODO: apply custom style to navbar links
 export const WebsiteNavbar: React.FC = () => {
+    const location = useLocation();
+
     return (
         <Navbar fluid rounded className="bg-secondary dark:bg-dark-secondary z-50 w-[100%] fixed top-0">
             <NavbarBrand>
@@ -18,30 +20,26 @@ export const WebsiteNavbar: React.FC = () => {
             </NavbarBrand>
             <NavbarToggle />
             <NavbarCollapse>
-                <div className="h-1/2 w-full flex">
-                    <ul className="h-full w-full flex">
-                        <li className={liStyle}>
-                            <Link to="/">
-                                <input className={buttonStyle} type="button" value="Home" />
-                            </Link>
-                        </li>
-                        <li className={liStyle}>
-                            <Link to="/games">
-                                <input className={buttonStyle} type="button" value="Games" />
-                            </Link>
-                        </li>
-                        <li className={liStyle}>
-                            <Link to="/projects">
-                                <input className={buttonStyle} type="button" value="Projects" />
-                            </Link>
-                        </li>
-                        <li className={liStyle}>
-                            <Link to="/about">
-                                <input className={buttonStyle} type="button" value="About" />
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+                <Link to="/">
+                    <NavbarLink active={location.pathname === '/'} as='div'>
+                        Home
+                    </NavbarLink>
+                </Link>
+                <Link to="/games">
+                    <NavbarLink active={location.pathname === '/games'} as='div'>
+                        Games
+                    </NavbarLink>
+                </Link>
+                <Link to="/projects">
+                    <NavbarLink active={location.pathname === '/projects'} as='div'>
+                        Projects
+                    </NavbarLink>
+                </Link>
+                <Link to="/about">
+                    <NavbarLink active={location.pathname === '/about'} as='div'>
+                        About
+                    </NavbarLink>
+                </Link>
             </NavbarCollapse>
         </Navbar>
     )
