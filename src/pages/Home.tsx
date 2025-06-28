1c4970c7cb752a094ef5f58771f9029bc30c7110
Primqd <franklinvchen@outlook.com>
2025-06-27 19:01:58 -0700

    adjusted padding and dimensions on home page for mobile compat

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,8 @@ import pig from "../assets/pig.png"
 const internalLinkStyle = "font-semibold text-text-highlight dark:text-dark-text-highlight hover:underline" // links to subpages
 
 export const Home: React.FC = () => {
-    return <div className="bg-primary text-text-highlight dark:bg-dark-primary dark:text-dark-text-highlight pt-[5rem] min-h-screen">
-        <div className="flex flex-col lg:flex-row justify-center items-center gap-[10vh] lg:gap-[25%] px-[5rem] pt-[8rem]">
+    return <div className="bg-primary text-text-highlight dark:bg-dark-primary dark:text-dark-text-highlight min-h-screen pt-[5rem] lg:pt-[12.5rem] px-[2rem]">
+        <div className="flex flex-col lg:flex-row justify-center items-center gap-[10vh] lg:gap-[25%]">
             <div className="yap align-middle">
                 <h1 className="text-5xl font-semibold pb-[0.75rem]">Hi!</h1>
                 <p className="max-w-lg text-lg text-text-default dark:text-dark-text-default pb-[0.75rem]">
