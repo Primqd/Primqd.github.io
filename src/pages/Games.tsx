0781b81bc0553cbcd882132e0d30e6a5f6165de4
Primqd <franklinvchen@outlook.com>
2025-07-22 23:51:16 -0700

    centered game

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -2,7 +2,7 @@ export const Games: React.FC = () => {
     return (
         <div className="bg-primary dark:bg-dark-primary pt-[5rem] min-h-screen items-center">
             <iframe
-                className=""
+                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
                 height={610}
                 width={1000}
                 scrolling="no"
