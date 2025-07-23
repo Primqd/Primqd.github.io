1fc8e6750a47bed11c647ffa61a71d6d55981ad1
Primqd <franklinvchen@outlook.com>
2025-07-22 22:47:08 -0700

    added functional iframe with game in games page

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,12 +1,13 @@
 export const Games: React.FC = () => {
     return (
         <div className="bg-primary dark:bg-dark-primary pt-[5rem] min-h-screen items-center">
-            <h1 className="text-text-default dark:text-dark-text-default py-[5rem] h-[80%] flex text-center items-center justify-center">
-                Nothing yet! Check back later...
-            </h1>
-            <h1 className="text-text-dark dark:text-dark-text-dark w-[80%] text-center italic absolute bottom-5 left-1/2 transform -translate-x-1/2">
-                Would you rather have unlimited bacon but no more video games or games, unlimited games, but no more games?
-            </h1>
+            <iframe
+                className=""
+                height={600}
+                width={960}
+                scrolling="no"
+                src="src\assets\operator_game\index.html"
+            />
         </div>
     );
 }
