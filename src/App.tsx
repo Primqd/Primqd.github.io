8055230a28374e6c654f4c0ee3a680b2bc980a9c
Primqd <franklinvchen@outlook.com>
2026-08-01 20:18:06 -0700

    removed unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './index.css'
 
 import Home from './pages/Home';
-import { Games } from './pages/Games';
-import { Projects } from './pages/Projects';
-import { About } from './pages/About';
 import { PhotoPortfolio } from './projects/photo-portfolio/PhotoPortfolio';
 
 function App() { // wrapper for app component to use useLocation
   return <Router>
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/games' element={<Games />} />
-      <Route path='/projects' element={<Projects />} />
       <Route path='/projects/photo-portfolio' element={<PhotoPortfolio />} />
-      <Route path='/about' element={<About />} />
     </Routes>
   </Router>
 }
